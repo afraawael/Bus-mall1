2619e748c3dcfc49cc755700717877a1dd947f66
afraa <afraawael96@gmail.com>
2021-04-19 14:39:57 +0300

    lab11

--- js/app.js
+++ js/app.js
@@ -2,24 +2,32 @@
 let leftImageElement = document.getElementById('left-image');
 let middleImageElement = document.getElementById('middle-image');
 let rightImageElement = document.getElementById('right-image');
+let container = document.getElementById('sec-one');
+
+
 
 let counts = 0;
 let maxAttempts =25;
 let leftIndex;
 let middleIndex;
 let rightIndex;
+let arrayOfnames = [];
+
 
 
 function Catalog(name, source){
   this.name = name;
   this.source =source;
   this.vote=0;
-  Catalog.allImages.push(this)
+  this.shown = 0;
+  Catalog.allImages.push(this);
+  arrayOfnames.push(this.name);
+
 }
 
+
+
 Catalog.allImages = [];
-
-
 new Catalog('bag','../img/bag.jpg');
 new Catalog('banana','../img/banana.jpg');
 new Catalog('bathroom','../img/bathroom.jpg');
@@ -43,65 +51,75 @@ new Catalog('wine-glass','../img/wine-glass.jpg');
 
 console.log(Catalog.allImages);
 
+
+
+
 function renderThreeImages (){
-    leftIndex = generateRandomIndex();
-    middleIndex = generateRandomIndex();
+  leftIndex = generateRandomIndex();
+  middleIndex = generateRandomIndex();
+  rightIndex = generateRandomIndex();
+  while(leftIndex === middleIndex || middleIndex === rightIndex || leftIndex === rightIndex){
     rightIndex = generateRandomIndex();
-    while(leftIndex === middleIndex || middleIndex === rightIndex || leftIndex === rightIndex){
-        rightIndex = generateRandomIndex();
-      }
-      
-   leftImageElement.src =  Catalog.allImages[leftIndex].source;
-   middleImageElement.src = Catalog.allImages[middleIndex].source;
-   leftImageElement.src =  Catalog.allImages[leftIndex].source;
- 
+    middleIndex = generateRandomIndex();
+
+  }
+
+  leftImageElement.src = Catalog.allImages[leftIndex].source;
+  Catalog.allImages[leftIndex].shown++;
+  middleImageElement.src = Catalog.allImages[middleIndex].source;
+  Catalog.allImages[middleIndex].shown++;
+  rightImageElement.src = Catalog.allImages[rightIndex].source;
+  Catalog.allImages[rightIndex].shown++;
 }
+
+
 
 renderThreeImages();
 
 
-
-leftImageElement.addEventListener('click', handleClicking);
-middleImageElement.addEventListener('click',handleClicking);
-rightImageElement.addEventListener('click',handleClicking);
+container.addEventListener('click',handleClicking);
 
 function handleClicking(event){
-      counts++;
-      if(maxAttempts >= counts){
-        if(event.target.id ==='left-image'){
-           Catalog.allImages[leftIndex].votes++;
-        }else if(event.target.id ==='middle-image'){
-            Catalog.allImages[middleIndex].votes++;
-         }else if(event.target.id ==='right-image'){
-              Catalog.allImages[rightIndex].votes++;
-      }
-      renderThreeImages();
-      console.log(Catalog.allImages);
+  counts++;
+  console.log(counts);
+  if(maxAttempts >= counts){
+    if(event.target.id ==='left-image'){
+      Catalog.allImages[leftIndex].vote++;
+    }else if(event.target.id ==='middle-image'){
+      Catalog.allImages[middleIndex].vote++;
+    }else if(event.target.id ==='right-image'){
+      Catalog.allImages[rightIndex].vote++;
     }else {
-
-      renderList()
-      leftImageElement.removeEventListener('click', handleClicking);
-      middleImageElement.removeEventListener('click',handleClicking);
-      rightImageElement.removeEventListener('click',handleClicking);
+      alert('Click on the image please!');
+      counts--;
     }
+    renderThreeImages();
+    console.log(Catalog.allImages);
+  }else {
+    renderList();
+    container.removeEventListener('click',handleClicking);
   }
-  
+}
 
 
-  function renderList(){
-    let ul = document.getElementById('list');
-    for(let i = 0 ; i < Catalog.allImages.length;i++){
-      let li = document.createElement('li');
-      ul.appendChild(li);
-      li.textContent = `${Catalog.allImages[i].name} it has ${Catalog.allImages[i].votes} Votes`;
-    }
+
+
+
+let arrayOfVotes = [];
+let arrayOfShown = [];
+function renderList(){
+  let ul = document.getElementById('list');
+  for(let i = 0 ; i < Catalog.allImages.length;i++){
+    arrayOfVotes.push(Catalog.allImages[i].vote);
+    arrayOfShown.push(Catalog.allImages[i].shown);
+    let li = document.createElement('li');
+    ul.appendChild(li);
+    li.textContent = `${Catalog.allImages[i].name} has ${Catalog.allImages[i].vote} votes and it shows ${Catalog.allImages[i].shown} times`;
   }
-  
+  console.log(Catalog.allImages);
 
-
+}
 
 function generateRandomIndex(){
-    return Math.floor(Math.random() * Catalog.allImages.length); 
- }
-
- 
+  return Math.floor(Math.random() * Catalog.allImages.length);
+}
